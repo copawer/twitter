05af7c56a29160916b4300de7d3a5f8eff4d5df4
Sara Gabriela Díaz Oporto <sara.diaz.oporto@gmail.com>
2017-02-08 10:20:53 -0500

    twitter

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
-var btn = document.getElementById('btn')
-var textarea=document.getElementById('textarea')
-var lista=document.getElementById('lista')
+
+
+   var listatareas={
+          {nombre:"tarea",isDone:true},
+          {nombre:"tarea2",isDone:false},
+           
+           
+       
+
+
+
+
+
+
+
+function drawtakslist ()
+{
+    var lista=document.getElementById("lista");
+    for(var i in listatareas){
+        var html="<li><input type='checkbox'"+(listatareas[i].isDone?"checked":""+">"+listatareas[i].nombre+"</li>";
+                lista.innerHTML += html                              
+                                               
+                                               }
+
+        }
+}
